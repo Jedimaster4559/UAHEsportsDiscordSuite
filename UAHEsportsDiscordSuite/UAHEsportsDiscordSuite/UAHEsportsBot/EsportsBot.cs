5446dd5be340a1c0fda72b740f8dfb7a9c38a941
Jedimaster4559 <nbsolomon4559@gmail.com>
2019-05-16 17:37:55 -0500

    added initial bot code for general bot

--- UAHEsportsDiscordSuite/UAHEsportsDiscordSuite/UAHEsportsBot/EsportsBot.cs
+++ UAHEsportsDiscordSuite/UAHEsportsDiscordSuite/UAHEsportsBot/EsportsBot.cs
@@ -1,12 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace UAHEsportsDiscordSuite.UAHEsportsBot
 {
     class EsportsBot
     {
+        static void runEsportsBot() => new EsportsBot().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection().AddSingleton(_client).AddSingleton(_commands).BuildServiceProvider();
+
+            PrivateKey key = JsonConvert.DeserializeObject<PrivateKey>(File.ReadAllText("PrivateKey.json"));
+
+            _client.Log += Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(Discord.TokenType.Bot, key.Key);
+
+            await _client.StartAsync();
+
+            await _client.SetGameAsync("&help");
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.Load(this.GetType().GetTypeInfo().Assembly.GetName()), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            var context = new SocketCommandContext(_client, message);
+
+            if (message is null || message.Author.IsBot || message.Author.IsWebhook) return;
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("&", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
+
+        public class PrivateKey
+        {
+            public string Key { get; set; }
+        }
     }
 }
