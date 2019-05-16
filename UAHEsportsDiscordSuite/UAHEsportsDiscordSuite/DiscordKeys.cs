10ee6e289b028895496684d3feb5c935763a58c6
Jedimaster4559 <nbsolomon4559@gmail.com>
2019-05-16 17:59:51 -0500

    Added static class to manage Discord Keys

---
+++ UAHEsportsDiscordSuite/UAHEsportsDiscordSuite/DiscordKeys.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAHEsportsDiscordSuite
+{
+    /// <summary>
+    /// DiscordKeys allows easy access to the Discord Keys for each bot this suite services. All Methods are static and no instance of this class is required.
+    /// </summary>
+    public class DiscordKeys
+    {
+        private string EsportsKey { get; }
+        private string RocketLeagueKey { get; }
+
+        private static DiscordKeys instance = null;
+        private static Object padlock = new Object();
+
+        /// <summary>
+        /// Gets the discord Keys object. This includes deserialization from 
+        /// </summary>
+        /// <returns>The instance of the Discord Keys</returns>
+        private static DiscordKeys GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (padlock)
+                {
+                    if(instance == null)
+                    {
+                        instance = JsonConvert.DeserializeObject<DiscordKeys>(File.ReadAllText("DiscordKeys.json"));
+                    }
+                }
+            }
+            return instance;
+            
+        }
+
+        /// <summary>
+        /// Gets the Esports Key for the main Esports Bot
+        /// </summary>
+        /// <returns>The Esports Discord Key</returns>
+        public static string GetEsportsKey()
+        {
+            return DiscordKeys.GetInstance().EsportsKey;
+        }
+
+        /// <summary>
+        /// Gets the Rocket League Discord Key for the Rocket League Discord Bot
+        /// </summary>
+        /// <returns>The Rocket League Discord Bot Key</returns>
+        public static string GetRocketLeagueKey()
+        {
+            return DiscordKeys.GetInstance().RocketLeagueKey;
+        }
+    }
+
+}
