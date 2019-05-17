eb7b3fef561b56d0f88fe016eba438eff7a6bbf3
Jedimaster4559 <nbsolomon4559@gmail.com>
2019-05-17 01:15:05 -0500

    added Voice Module to Rocket League

---
+++ UAHEsportsDiscordSuite/UAHEsportsDiscordSuite/RocketLeagueBot/Commands/Voice.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+
+namespace UAHEsportsDiscordSuite.RocketLeagueBot.Commands
+{
+
+    public class Voice : ModuleBase<SocketCommandContext>
+    {
+        [Command("lock")]
+        public async Task LockAsync()
+        {
+            var user = Context.User;
+            var vc = (user as IVoiceState)?.VoiceChannel;
+            if (vc != null)
+            { // Make sure the users is actually in a voice channel
+                var count = (await vc.GetUsersAsync().FlattenAsync()).Count(); // Get number of users in the current voice channel
+                await vc.ModifyAsync(x =>
+                {
+                    x.UserLimit = count; // Set the user limit of the channel equal to the current number of users
+                });
+                Console.WriteLine($"{user.Username}#{user.Discriminator} locked {vc.Name} with {count} users");
+                await Context.Channel.SendMessageAsync($"{vc.Name} has been locked by " + user.Mention);
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("You must be in a voice channel to perform this command");
+            }
+        }
+
+        [Command("unlock")]
+        public async Task UnLockAsync()
+        {
+            var user = Context.User;
+            var vc = ((IVoiceState)user).VoiceChannel;
+            if (vc != null)
+            {
+                await vc.ModifyAsync(x =>
+                {
+                    x.UserLimit = 0;
+                });
+                Console.WriteLine($"{user.Username}#{user.Discriminator} unlocked {vc.Name}");
+                await Context.Channel.SendMessageAsync($"{vc.Name} has been unlocked by " + user.Mention);
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("You must be in a voice channel to perform this command");
+            }
+        }
+
+
+        // Takes a voice channel name and returns the base name, and the number of the channel
+        public static VoiceChannelName ProcessName(string name)
+        {
+            var parts = name.Split(' '); // Separate the name at spaces and storing into an array\
+            var cat = string.Join(" ", parts.Take(parts.Length - 1)); // Joins all parts except the last back into a string
+            var isNum = int.TryParse(parts[parts.Length - 1], out var num); // Attempts to convert the last part to an integer
+            if (isNum) // Check if the last part is an integer
+            {
+                return new VoiceChannelName(cat, num);
+            }
+            cat = cat + " " + (parts[parts.Length - 1]); // The last part is not a number so add it back on
+            return new VoiceChannelName(cat.Trim(), 0);
+        }
+
+    }
+
+    public class VoiceChannelName
+    {
+        public string name { get; set; }
+        public int number { get; set; }
+
+        public VoiceChannelName(string name, int number)
+        {
+            this.name = name;
+            this.number = number;
+        }
+    }
+}
--- UAHEsportsDiscordSuite/UAHEsportsDiscordSuite/RocketLeagueBot/RocketLeagueBot.cs
+++ UAHEsportsDiscordSuite/UAHEsportsDiscordSuite/RocketLeagueBot/RocketLeagueBot.cs
@@ -39,6 +39,8 @@ namespace UAHEsportsDiscordSuite.RocketLeagueBot
 
             _client.Log += Log;
 
+            await SubscribeToEvents();
+
             await RegisterCommandsAsync();
 
             await _client.LoginAsync(Discord.TokenType.Bot, DiscordKeys.GetRocketLeagueKey());
@@ -62,16 +64,22 @@ namespace UAHEsportsDiscordSuite.RocketLeagueBot
             return Task.CompletedTask;
         }
 
+        public async Task SubscribeToEvents()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            _client.UserVoiceStateUpdated += UpdateVoiceChannels;
+        }
+
         /// <summary>
         /// Subscribe to received messages and register all of the commands
         /// </summary>
         /// <returns></returns>
         public async Task RegisterCommandsAsync()
         {
-            _client.MessageReceived += HandleCommandAsync;
-
+            // Add Commands to this by adding a new line with the Class between the <>
             await _commands.AddModuleAsync<Commands.Ping>(_services);
-            //await _commands.AddModulesAsync(Assembly.Load(this.GetType().GetTypeInfo().Assembly.GetName()), _services);
+
         }
 
         /// <summary>
@@ -96,6 +104,96 @@ namespace UAHEsportsDiscordSuite.RocketLeagueBot
                 if (!result.IsSuccess)
                 {
                     Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
+
+        private static async Task UpdateVoiceChannels(SocketUser su, SocketVoiceState oldState, SocketVoiceState newState)
+        {
+            string[] whitelist = { "General", "Gaming", "Chillin'" }; // List of channels not to modify
+            if (newState.VoiceChannel == oldState.VoiceChannel) return; // The event can be triggered without changing channels
+
+            // Handle joining a channel
+            if (newState.VoiceChannel != null && whitelist.Contains(Commands.Voice.ProcessName(newState.VoiceChannel.Name).name)) // Check to see if a channel was joined and that channel is not in the blacklist
+            {
+                IGuildChannel vc = newState.VoiceChannel; // 
+                var vct = Commands.Voice.ProcessName(vc.Name);
+                Console.WriteLine($"{su.Username}#{su.Discriminator} has joined {vc.Name}({vc.Id})");
+
+                var users = (await vc.GetUsersAsync().FlattenAsync()).ToList();
+
+                if (users.Count == 1) // If there is only one person in the group, it was previously empty, and another slot needs opened up
+                {
+                    var pos = 0;
+                    var number = 1;
+                    var vcs = await vc.Guild.GetVoiceChannelsAsync(); // Get a list of voice channels
+                    var vcso = vcs.OrderBy(item => item.Position); // Order the list of voice channels
+                    foreach (var channel in vcso) // Loop over every channel
+                    {
+                        var chnl = Commands.Voice.ProcessName(channel.Name); // Get the name and number from the channel
+                        if (chnl.name != vct.name) continue; // Check to see if the channel is of the same type
+                        pos = Math.Max(channel.Position, pos); // If the channel is lower, save the new position
+                        if (chnl.number != number)
+                        { // Is the channel number correct
+                            await ((SocketVoiceChannel)channel).ModifyAsync(x =>
+                            {
+                                // ReSharper disable once AccessToModifiedClosure
+                                x.Name = chnl.name + " " + number; // Update the channel number
+                            });
+                        }
+                        number++; // Increment the number that the channel is supposed to be
+                    }
+
+                    var rcpe = (await vc.Guild.GetVoiceChannelsAsync()).Select(channel => new ReorderChannelProperties(channel.Id, channel.Position + (channel.Position > pos ? 1 : 0))).ToList(); // Enumerable to store the new channel order
+
+
+                    var nvc = await vc.Guild.CreateVoiceChannelAsync(vct.name + " " + number); // Create the channel
+                    Console.WriteLine($"Created channel {nvc.Name}({nvc.Id})");
+                    await nvc.ModifyAsync(x =>
+                    {
+                        x.CategoryId = ((INestedChannel)vc).CategoryId;
+                        x.Position = pos + 1; // Set the position of the channel
+                    });
+                    rcpe.Add(new ReorderChannelProperties(nvc.GuildId, pos + 1)); // Add the channel to the list of channels to reorder
+
+                    await vc.Guild.ReorderChannelsAsync(rcpe); // Reorder the channels
+                }
+            }
+
+            // Handle leaving a channel
+            if (oldState.VoiceChannel != null && whitelist.Contains(Commands.Voice.ProcessName(oldState.VoiceChannel.Name).name))
+            {
+                IGuildChannel vc = oldState.VoiceChannel;
+                var vct = Commands.Voice.ProcessName(vc.Name);
+                Console.WriteLine($"{su.Username}#{su.Discriminator} has left {vc.Name}({vc.Id})");
+
+                var users = (await vc.GetUsersAsync().FlattenAsync()).ToList();
+
+                if (!users.Any()) // If there are no users in the channel
+                {
+                    Console.WriteLine($"Removing empty channel {vc.Name}({vc.Id})");
+                    await vc.DeleteAsync(); // Delete it
+
+                    var number = 1;
+                    var vcs = await vc.Guild.GetVoiceChannelsAsync();
+                    var vcso = vcs.OrderBy((item) => item.Position);
+
+                    // This functions almost identically to the number adjustment in the join
+                    foreach (var channel in vcso)
+                    {
+                        var chnl = Commands.Voice.ProcessName(channel.Name);
+                        if (chnl.name != vct.name) continue;
+                        if (chnl.number == vct.number) continue; // Because the deleted channel is still in the list, skip it
+                        if (chnl.number != number)
+                        {
+                            await ((SocketVoiceChannel)channel).ModifyAsync(x =>
+                            {
+                                // ReSharper disable once AccessToModifiedClosure
+                                x.Name = chnl.name + " " + number;
+                            });
+                        }
+                        number++;
+                    }
                 }
             }
         }
