8756c99cf6982bff5ad507bbf077d9cf5e2fa0a1
Jedimaster4559 <nbsolomon4559@gmail.com>
2019-05-18 19:26:37 -0500

    some helpful methods for role management

---
+++ UAHEsportsDiscordSuite/UAHEsportsDiscordSuite/Utilities/RoleHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.Net;
+
+namespace UAHEsportsDiscordSuite.Utilities
+{
+    public class RoleHelper
+    {
+        public static ulong getRoleID(string roleName, SocketCommandContext Context)
+        {
+            IRole role = getRole(roleName, Context);
+            return role.Id;
+        }
+
+        public static IRole getRole(string roleName, SocketCommandContext Context)
+        {
+            var roles = Context.Guild.Roles;
+            roleName = roleName.ToLower();
+
+            IRole returnRole = null;
+
+            foreach(IRole role in roles)
+            {
+                if(role.Name.ToLower() == roleName)
+                {
+                    returnRole = role;
+                    break;
+                }
+            }
+
+            return returnRole;
+        }
+    }
+}
