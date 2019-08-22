1f90f99874ff42f49f641be173e9c78dc6e7e83d
Nathan Solomon <nbsolomon4559@gmail.com>
2019-08-21 22:53:48 -0500

    added owner level tools for bot setup purposes

---
+++ UAHEsportsDiscordSuite/UAHEsportsDiscordSuite/UAHEsportsBot/BotUserControls/Reactions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+
+namespace UAHEsportsDiscordSuite.UAHEsportsBot.BotUserControls
+{
+    [Group("React")]
+    [Alias("react")]
+    [PermissionSets.BotOwnerPermissions]
+    public class Reactions : ModuleBase<SocketCommandContext>
+    {
+        [Command("Add")]
+        [Alias("add")]
+        public async void add(ulong messageID, IEmote emoji)
+        {
+            IUserMessage message = await Context.Channel.GetMessageAsync(messageID) as IUserMessage;
+            await message.AddReactionAsync(emoji);
+            await ReplyAsync($"{Context.User.Mention} Your reaction has been added!");
+        }
+    }
+}
